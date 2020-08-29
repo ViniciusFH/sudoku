542beda05a0ce645b974481ab125d6fb9e1027cb
ViniciusFH <vinicius.froncillo@gmail.com>
2020-08-29 14:15:07 -0300

    init

---
+++ puzzle/board.js
@@ -0,0 +1,1154 @@
+module.exports = [
+  {
+    "sectors": [
+      "b1",
+      "c1",
+      "r1"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "5",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c2",
+      "r1"
+    ],
+    "value": "",
+    "possibilities": [
+      "5",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c3",
+      "r1"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "5",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c1",
+      "r2"
+    ],
+    "value": "4",
+    "possibilities": [
+      "2",
+      "3",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c2",
+      "r2"
+    ],
+    "value": "",
+    "possibilities": [
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c3",
+      "r2"
+    ],
+    "value": "6",
+    "possibilities": [
+      "2",
+      "3",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c1",
+      "r3"
+    ],
+    "value": "2",
+    "possibilities": [
+      "3",
+      "5",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c2",
+      "r3"
+    ],
+    "value": "1",
+    "possibilities": [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b1",
+      "c3",
+      "r3"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "5",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c4",
+      "r1"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c5",
+      "r1"
+    ],
+    "value": "8",
+    "possibilities": [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c6",
+      "r1"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "5",
+      "6",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c4",
+      "r2"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "3"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c5",
+      "r2"
+    ],
+    "value": "3",
+    "possibilities": []
+  },
+  {
+    "sectors": [
+      "b2",
+      "c6",
+      "r2"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "3",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c4",
+      "r3"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "4"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c5",
+      "r3"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "4",
+      "5"
+    ]
+  },
+  {
+    "sectors": [
+      "b2",
+      "c6",
+      "r3"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "4",
+      "5",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c7",
+      "r1"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c8",
+      "r1"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "3",
+      "7"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c9",
+      "r1"
+    ],
+    "value": "4",
+    "possibilities": [
+      "1",
+      "2",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c7",
+      "r2"
+    ],
+    "value": "5",
+    "possibilities": [
+      "1",
+      "2",
+      "6",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c8",
+      "r2"
+    ],
+    "value": "2",
+    "possibilities": [
+      "1",
+      "3",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c9",
+      "r2"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c7",
+      "r3"
+    ],
+    "value": "",
+    "possibilities": [
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c8",
+      "r3"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b3",
+      "c9",
+      "r3"
+    ],
+    "value": "6",
+    "possibilities": [
+      "2",
+      "4",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c1",
+      "r4"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "3",
+      "6",
+      "7"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c2",
+      "r4"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "7"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c3",
+      "r4"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "7"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c1",
+      "r5"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "5",
+      "6",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c2",
+      "r5"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "4",
+      "5",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c3",
+      "r5"
+    ],
+    "value": "8",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c1",
+      "r6"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "5",
+      "6",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c2",
+      "r6"
+    ],
+    "value": "",
+    "possibilities": [
+      "4",
+      "5",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b4",
+      "c3",
+      "r6"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "4",
+      "5",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c4",
+      "r4"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "3",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c5",
+      "r4"
+    ],
+    "value": "9",
+    "possibilities": [
+      "2",
+      "3",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c6",
+      "r4"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "3",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c4",
+      "r5"
+    ],
+    "value": "7",
+    "possibilities": [
+      "2",
+      "4",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c5",
+      "r5"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "4",
+      "5",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c6",
+      "r5"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "4",
+      "5",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c4",
+      "r6"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "4",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c5",
+      "r6"
+    ],
+    "value": "1",
+    "possibilities": [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b5",
+      "c6",
+      "r6"
+    ],
+    "value": "",
+    "possibilities": [
+      "3",
+      "4",
+      "5",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c7",
+      "r4"
+    ],
+    "value": "4",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "5",
+      "6",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c8",
+      "r4"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c9",
+      "r4"
+    ],
+    "value": "5",
+    "possibilities": [
+      "1",
+      "2",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c7",
+      "r5"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "6",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c8",
+      "r5"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c9",
+      "r5"
+    ],
+    "value": "3",
+    "possibilities": [
+      "1",
+      "2",
+      "5",
+      "6",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c7",
+      "r6"
+    ],
+    "value": "7",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c8",
+      "r6"
+    ],
+    "value": "",
+    "possibilities": [
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b6",
+      "c9",
+      "r6"
+    ],
+    "value": "2",
+    "possibilities": [
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c1",
+      "r7"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "5",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c2",
+      "r7"
+    ],
+    "value": "6",
+    "possibilities": [
+      "2",
+      "3",
+      "4",
+      "5",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c3",
+      "r7"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c1",
+      "r8"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c2",
+      "r8"
+    ],
+    "value": "3",
+    "possibilities": [
+      "2",
+      "4",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c3",
+      "r8"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "4",
+      "7",
+      "9"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c1",
+      "r9"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "5",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c2",
+      "r9"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "4",
+      "5",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b7",
+      "c3",
+      "r9"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "4",
+      "5",
+      "7"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c4",
+      "r7"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c5",
+      "r7"
+    ],
+    "value": "7",
+    "possibilities": [
+      "2",
+      "3",
+      "4",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c6",
+      "r7"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c4",
+      "r8"
+    ],
+    "value": "5",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c5",
+      "r8"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "4",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c6",
+      "r8"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "4",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c4",
+      "r9"
+    ],
+    "value": "9",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c5",
+      "r9"
+    ],
+    "value": "",
+    "possibilities": [
+      "2",
+      "4",
+      "6"
+    ]
+  },
+  {
+    "sectors": [
+      "b8",
+      "c6",
+      "r9"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "4",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c7",
+      "r7"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c8",
+      "r7"
+    ],
+    "value": "9",
+    "possibilities": [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c9",
+      "r7"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c7",
+      "r8"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "2",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c8",
+      "r8"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "4",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c9",
+      "r8"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c7",
+      "r9"
+    ],
+    "value": "3",
+    "possibilities": [
+      "1",
+      "2",
+      "5",
+      "6",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c8",
+      "r9"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+  {
+    "sectors": [
+      "b9",
+      "c9",
+      "r9"
+    ],
+    "value": "",
+    "possibilities": [
+      "1",
+      "7",
+      "8"
+    ]
+  }
+]
